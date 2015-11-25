a8d3ca09d217932a763ceb749ea098337af8d6fa
jingtao <jingtao@smartisan.cn>
2015-11-25 20:45:08 +0800

    add tracing logic

--- src/ireader/home/Home.java
+++ src/ireader/home/Home.java
@@ -14,6 +14,7 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.Debug;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ListView;
@@ -29,6 +30,7 @@ public class Home extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Debug.startMethodTracing("iread");
         setContentView(R.layout.main);
         getAllApp();
         mAppListAdapter = new AppListAdapter(this, mAllApps);
@@ -46,6 +48,7 @@ public class Home extends Activity {
         registerReceiver(packageReceiver, filter);
 
         EventBus.getDefault().register(this);
+        Debug.stopMethodTracing();
     }
 
     @Override
